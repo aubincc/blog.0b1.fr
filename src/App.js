d3b9e3bbfb5021e093b4f59e712273560ee688c1
Mole <git@molobox.com>
2022-07-29 01:35:21 +0200

    moving html content into src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const App = () => {
+  return (
+    <video autoplay="true" muted="true" loop="true" id="myVideo">
+      <source src="loop.mp4" type="video/mp4" />
+    </video>
+  )
+}
+
+export default App
